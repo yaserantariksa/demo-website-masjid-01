0cbb006a450a83dbee3a78edfdfaa77f1ad2a155
yaserantariksa <yaserant@gmail.com>
2024-05-04 15:42:36 +0700

    initial commit

---
+++ src/app/web-config.ts
@@ -0,0 +1,12 @@
+export const webConfig = {
+	baseUrl: "http://localhost:3000",
+	title: "Website Masjid Al Fithroh",
+	description:
+		"Website Masjid Al Fithroh: informasi kajian, kegiatan keagamaan, dan donasi",
+	authors: {
+		name: "Yaser Antariksa",
+		url: "https://yaserantariksa.vercel.app",
+	},
+	keywords: ["masjid al-fithroh", "kajian", "pengajian", "ibadah"],
+	creator: "Yaser Antariksa",
+};
